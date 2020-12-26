428ee3ddda009be99436b939605fce418515d7a9
Ryan Pang <ryan.pang@live.com>
2020-12-26 21:53:02 +1300

    Update

--- target/classes/META-INF/frontend/vaadin-password-strength.js
+++ target/classes/META-INF/frontend/vaadin-password-strength.js
@@ -1,4 +1,4 @@
-import {css, html, LitElement} from "lit-element";
+import {html, LitElement} from "lit-element";
 import '@collaborne/polymer-password-strength';
 
 
@@ -18,31 +18,6 @@ class VaadinPasswordStrength extends LitElement {
 
     createRenderRoot() {
         return this;
-    }
-
-    static get styles() {
-        return css `
-			:host {
-				display: block;
-				width: 180px;
-			}
-
-			#back {
-				background: #eee;
-			}
-
-			#front {
-				height: 5px;
-				transition: all .5s;
-			}
-
-			:host([score="0"]) #front { width: 0; }
-			:host([score="1"]) #front { background: red; width: 20%; }
-            :host([score="2"]) #front { background: orange; width: 40%; }
-            :host([score="3"]) #front { background: yellow; width: 60%; }
-            :host([score="4"]) #front { background: greenyellow; width: 80%; }
-            :host([score="5"]) #front { background: green; width: 100%; }
-		`;
     }
 
     render() {
